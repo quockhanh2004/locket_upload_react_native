b1cb6805e1d5e5f21e7b27523029741fee0b68a5
Phạm Ngọc Quốc Khánh <phamngocquockhanh2004@gmail.com>
2025-01-17 21:37:10 +0700

    Build and release APK version 0.0.2 on 20250117_2136

--- src/redux/action/user.action.js
+++ src/redux/action/user.action.js
@@ -123,7 +123,7 @@ export const getToken = createAsyncThunk(
         refreshToken,
       };
       const response = await axios.post(
-        'https://securetoken.googleapis.com/v1/token',
+        'https://securetoken.googleapis.com/v1/token?key=AIzaSyCQngaaXQIfJaH0aS2l7REgIjD7nL431So',
         body,
         {headers: loginHeader},
       );
