95a6b6c47345f33fff2c266becdfa7337f8dcba5
Phạm Ngọc Quốc Khánh <phamngocquockhanh2004@gmail.com>
2025-04-03 20:20:27 +0700

    tăng tốc độ lấy danh sách bạn bè

--- src/util/Friends.tsx
+++ src/util/Friends.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable no-async-promise-executor */
 import axios from 'axios';
 import {Friend} from '../redux/slice/friends.slice';
 
@@ -12,28 +13,48 @@ export const getListIdFriend = async (token: string, userId: string) => {
   return response.data.users;
 };
 
-export const getListFriend = async (token: string, listIdFriend: string[]) => {
-  const friends: Friend[] = [];
-  for (const friend of listIdFriend) {
-    const response = await axios.post(
-      'https://api.locketcamera.com/fetchUserV2',
-      {
-        data: {
-          user_uid: friend,
-        },
-      },
-      {
-        headers: {
-          Authorization: `Bearer ${token}`,
-          'Content-Type': 'application/json',
-        },
-      },
-    );
+export const getListFriend = (
+  token: string,
+  listIdFriend: string[],
+): Promise<Friend[]> => {
+  const friendPromises = listIdFriend.map(friendId => {
+    return new Promise<Friend>(async (resolve, reject) => {
+      try {
+        const response = await axios.post(
+          'https://api.locketcamera.com/fetchUserV2',
+          {
+            data: {
+              user_uid: friendId,
+            },
+          },
+          {
+            headers: {
+              Authorization: `Bearer ${token}`,
+              'Content-Type': 'application/json',
+            },
+          },
+        );
 
-    const data = response.data.result.data as Friend;
+        const data = response.data.result.data as Friend;
 
-    friends.push(data);
-  }
+        // Kiểm tra dữ liệu hợp lệ trước khi resolve promise
+        if (data && data.uid) {
+          console.log(data);
+          resolve(data); // Resolve với dữ liệu bạn bè
+        } else {
+          reject(new Error(`Friend data for ${friendId} is invalid.`));
+        }
+      } catch (error) {
+        reject(error); // Reject với lỗi nếu có
+      }
+    });
+  });
 
-  return friends;
+  // Chờ tất cả các promises hoàn thành và trả về mảng bạn bè
+  return Promise.all(friendPromises)
+    .then(friends => friends)
+    .catch(error => {
+      console.error('Error fetching friends:', error);
+      throw error;
+    });
 };
